0a0b17b0aa5782d7429bb9e85b4243114f903a48
SoieYoo <yoosoie0000@gmail.com>
2024-04-24 23:52:27 +0900

    Initial commit

---
+++ src/main/java/com/zerobase/mytabling/MyTablingApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.mytabling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTablingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTablingApplication.class, args);
+    }
+
+}
